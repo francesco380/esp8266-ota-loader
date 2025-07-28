a2d54aaf8daca7c4dd0dd97f7366bd96080081aa
francesco380 <francesco380@gmail.com>
2025-07-28 15:38:49 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,42 @@
-extern "C" {
-  #include "user_interface.h"
-}
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
-#include <ESP8266HTTPUpdateServer.h>
+#include <Updater.h>
 
 const char* ssid = "NOME_WIFI";
 const char* password = "PASSWORD_WIFI";
 
 ESP8266WebServer server(80);
-ESP8266HTTPUpdateServer httpUpdater;
+
+void handleRoot() {
+  server.send(200, "text/html", "<form method='POST' action='/update' enctype='multipart/form-data'>"
+                                "<input type='file' name='update'>"
+                                "<input type='submit' value='Aggiorna'>"
+                                "</form>");
+}
+
+void handleUpdate() {
+  HTTPUpload& upload = server.upload();
+  if (upload.status == UPLOAD_FILE_START) {
+    Update.begin(UPDATE_SIZE_UNKNOWN);
+  } else if (upload.status == UPLOAD_FILE_WRITE) {
+    Update.write(upload.buf, upload.currentSize);
+  } else if (upload.status == UPLOAD_FILE_END) {
+    if (Update.end(true)) {
+      server.send(200, "text/plain", "Update Success! Rebooting...");
+      ESP.restart();
+    } else {
+      server.send(500, "text/plain", "Update Failed");
+    }
+  }
+}
 
 void setup() {
-  wifi_set_opmode(STATION_MODE);
+  WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(100);
-  }
-  httpUpdater.setup(&server);
+  while (WiFi.status() != WL_CONNECTED) delay(100);
+
+  server.on("/", HTTP_GET, handleRoot);
+  server.on("/update", HTTP_POST, []() { server.send(200); }, handleUpdate);
   server.begin();
 }
 
