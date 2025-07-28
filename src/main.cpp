3abf3177dc173c37341088fdc73b42b63aca5b5d
francesco380 <francesco380@gmail.com>
2025-07-28 15:26:23 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,21 @@
+#include <Arduino.h>
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
 #include <ESP8266HTTPUpdateServer.h>
+
+const char* ssid = "NOME_WIFI";
+const char* password = "PASSWORD_WIFI";
 
 ESP8266WebServer server(80);
 ESP8266HTTPUpdateServer httpUpdater;
 
 void setup() {
-  Serial.begin(115200);
   WiFi.mode(WIFI_STA);
-  WiFi.begin();
-
-  server.on("/", []() {
-    server.send(200, "text/html", "<h1>OTA Loader</h1><p><a href='/update'>Carica Firmware</a></p>");
-  });
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) delay(100);
 
   httpUpdater.setup(&server);
   server.begin();
-
-  Serial.println("OTA Loader started");
 }
 
 void loop() {
